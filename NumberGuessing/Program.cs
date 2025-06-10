dc69235435d2c267e05f9c3399e78a6fa11f8ab5
Andri Manuel Zabala Urbaez <andriurbaez05@gmail.com>
2025-06-10 16:43:32 -0400

    Cambio de nombre de todos los metodos excepto por el Main, volvi los metodos Responde y MaxAttempts mas sencillos, quite las palabras en español, agregue nuevo metodo ShowLossMessage, ShowBeasts y WelcomeMessage, cambie la logica para adivinar los numeros.

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -1,69 +1,123 @@
-﻿using System.Threading.Channels;
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+using System.Timers;
 using System.Transactions;
 using System.Xml.Serialization;
 
 class Program
 {
-    static int number() =>
-        new Random().Next(1, 100);
+    static Random random = new();
+    static Stopwatch timer = new();
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Number Guessing Game!");
-        //Thread.Sleep(1500); //Espera un segundo y medio.
-        Console.WriteLine("I'm thinking of a number between 1 and 100.");
-        //Thread.Sleep(1500); //Espera un segundo y medio.
-        Console.WriteLine("You have 5 chances to guess the correct number.\n");
+        WelcomeMessage(); //Show Welcome Messages.
+        List<int> attemptsList = new List<int>();
+        List<TimeSpan> timeList = [];
 
-        var num = number();
-        var choi = choice();
-        int input = 0, count = 0;
-
-        if (choi == 0)
-            return;
-
-        while (count < choi)
+        while (true)
         {
-            count++;
-            Console.WriteLine("Enter your guess:");
-            if (!int.TryParse(Console.ReadLine(), out input) && input > 0)
+            int input, count = 0;
+            var secretNumber = random.Next(1, 101);
+            var maxAttemps = MaxAttempts();
+            bool flag = false;
+
+            while (count < maxAttemps)
             {
-                Console.WriteLine("You must enter a valid number.");
-                return;
-            }
-            else
-            {
-                if (input == num)
+                timer.Start();
+                count++;
+                Console.WriteLine("\nEnter your guess:");
+                while (!int.TryParse(Console.ReadLine(), out input) || input <= 0)
                 {
-                    Console.WriteLine($"Congratulations! You guessed the correct number in {count} attempts.");
-                    return;
+                    Console.WriteLine("You must enter a valid number.");
                 }
-                Console.WriteLine(input > num ? "Incorrect! The number is less than {0}" :
-                    "Incorrect! The number is greater than {0}", input);
+                    if(input == secretNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine($"Congratulations! You guessed the correct number in {count} attempts.");
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        attemptsList.Add(count);
+                        flag = true; 
+                        
+                        timer.Stop(); 
+                        timeList.Add(timer.Elapsed); 
+                        Console.WriteLine($"Tiempo transcurrido:{timer.Elapsed}"); 
+                        timer.Reset();
+                        break; 
+                    }
+                    else
+                    {
+                        Console.WriteLine(input > secretNumber ? "Incorrect! The number is less than {0}" :
+                        "Incorrect! The number is greater than {0}", input);
+                    }
             }
+            ShowLossMessage(flag, secretNumber); 
+            if (Response() == 2)
+                break;
         }
-        Console.WriteLine($"Game over! the was number is {num}.");
+        if (attemptsList.Any() && timeList.Any())
+            ShowBests(attemptsList,timeList);
     }
-    static int choice()
+    static void WelcomeMessage()
     {
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        Thread.Sleep(1500); 
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+        Thread.Sleep(1500); 
+        Console.WriteLine("You have 5 chances to guess the correct number.\n");
+    }
+    static void ShowBests(List<int> attemptsList, List<TimeSpan> timeList)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Best attemp: {attemptsList.Min()}");
+        Console.WriteLine($"Best Time Elapsed: {timeList.Min()}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    static void ShowLossMessage (bool flag, int secretNumber) 
+    {
+        if(flag != true)
+        {
+            timer.Stop();
+
+            Console.WriteLine($"Elapsed time:{timer.Elapsed}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Game over! the number was {secretNumber}.");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            timer.Reset();
+        }
+    }
+    static int Response()
+    {
+        int response;
+        Console.WriteLine("1. Continue the game. \n2. Exit.");
+        while (!int.TryParse(Console.ReadLine(), out response) || response < 1 || response > 2)
+        {
+            Console.WriteLine("Invalid input. Please enter 1 or 2:");
+        }
+        return response;
+    }
+    static int MaxAttempts()
+    {
+        int choice;
         Console.WriteLine("Please select the dificulty level:");
         Console.WriteLine(" 1. Easy (10 chances) \n 2. Medium (5 chances) \n 3. Hard (3 chances) \n");
+        Thread.Sleep(1500);
 
         Console.WriteLine("Enter your choice:");
-        if (!int.TryParse(Console.ReadLine(), out int choice))
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice <1 || choice >3)
         {
-            Console.WriteLine("Tiene que ingresar un numero entre 1 y 3.");
-            return 0;
+            Console.WriteLine("Invalid input. Please enter number between 1 and 3:");
         }
-        else
+        return choice switch
         {
-            return choice switch
-            {
-                1 => 10,
-                2 => 5,
-                3 => 3,
-                _ => 0
-            };
-        }
+            1 => 10,
+            2 => 5,
+            3 => 3,
+            _ => 0
+        };
     }
-    
 }
