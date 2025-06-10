3e7b01239be0626b3ee6558540c38c25c916f327
Andri Manuel Zabala Urbaez <andriurbaez05@gmail.com>
2025-06-09 21:38:06 -0400

    Primer push hacia el repositorio remoto

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public void adivinar()
+    {
+        var num = numero();
+        int entrada = 0;
+        do
+        {
+            if (int.TryParse(Console.ReadLine(), out entrada))
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        while (num != entrada);
+    }
+    int numero()
+    {
+        var num = new Random().Next(1, 100);
+        return num;
+    }
+}
